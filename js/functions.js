b2959cc11df0117df1f9fc0abbb41417cd781c45
Clément Prod'homme <clement.prodhomme@gmail.com>
2014-07-29 16:52:43 -0400

    Add
    Hovering the name of the communities highlight them on the map

--- js/functions.js
+++ js/functions.js
@@ -61,23 +61,40 @@ function getUrbanAreaData(communityName) {
     return null;
 }
 
-function displayUrbanArea(nb, total) {
-    //We retrieve the data from urbanAreaFeature
+//Return the layer Id of the urban area
+function getUrbanAreaLayerId(urbanAreaName) {
     var layers = urbanAreaFeature.getLayers();
     for(var j = 0; j < layers.length; j++) {
-        if(layers[j].feature.properties.TOWN.toLowerCase().trim() == communityName) {
-            urbanAreaLayerID = layers[j]._leaflet_id;
+        if(layers[j].feature.properties.TOWN.toLowerCase().trim() == urbanAreaName) {
+            return layers[j]._leaflet_id;
         }
     }
+}
+
+//Set the color and the opacity of the fill of a urban area
+function setUrbanAreaLayerfillColor(urbanAreaLayerId, color, opacity) {
+    urbanAreaFeature.getLayer(urbanAreaLayerId).setStyle({
+        fillColor: color,
+        fillOpacity: opacity
+    });
+}
+
+//Set the color and the opacity of the stroke of a urban area
+function setUrbanAreaLayerstrokeColor(urbanAreaLayerId, color, opacity, width) {
+    urbanAreaFeature.getLayer(urbanAreaLayerId).setStyle({
+        color: color,
+        opacity: opacity,
+        weight: width
+    });
+}
+
+function displayUrbanArea(communityName, nb, total) {
+    var urbanAreaLayerId = getUrbanAreaLayerId(communityName);
 
     //We change the color of the layer
-    var colorScale = chroma.scale(["#FFDEE3", "#590C19"]).domain([0, total]).out("hex");
-    if(urbanAreaLayerID != null) {
-        urbanAreaFeature.getLayer(urbanAreaLayerID).setStyle({
-            fillColor: colorScale(nb),
-            fillOpacity: .8
-        });
-    }
+    var colorScale = chroma.scale(["#61C280", "#C41212"]).domain([0, total]).out("hex");
+    if(urbanAreaLayerId != null)
+        setUrbanAreaLayerfillColor(urbanAreaLayerId, colorScale(nb), .8);
 }
 
 //Display the top communities for an hospital
@@ -85,15 +102,84 @@ function topCommunityFromHospital(d) {
     //We get the urban areas where the people come from
     var hospital = hospitalsData[d.id - 1];
     
-    //We display a panel
-    if(d3.select(".panel").length > 0) {
+    //We eventually remove the old panel
+    if(d3.select(".panel")[0][0]) {
         d3.select(".panel")
           .remove();
         map.eachLayer(function(layer) {
             urbanAreaFeature.resetStyle(layer);
         });
     }
+    
+//    var popup = L.popup({closeOnClick: false, closeButton: true})
+//      .setLatLng([d.latitude, d.longitude])
+//      .setContent(d.name)
+//      .openOn(map);
+//    
+    
+    
+    //We calculate the number of patients of that hospital
+    var totalHospital = 0;
+    var urbanAreasLayerId = [];
+    for(var i = 0; i < hospital.topCommunities.length; i++)
+        totalHospital += hospital.topCommunities[i].patients;
+    
+    for(var i = 0; i < hospital.topCommunities.length; i++) {
         
+        //Only in the case the city in inside MA
+        if(hospital.topCommunities[i].state == "MA") {
+            var nbHospital =  hospital.topCommunities[i].patients;
+            communityName = hospital.topCommunities[i].name.toLowerCase().trim();
+            
+            //We retrieve the data from urbanAreaData
+            urbanAreaObject = getUrbanAreaData(communityName);
+            urbanAreasLayerId.push(getUrbanAreaLayerId(communityName));
+            displayUrbanArea(communityName, nbHospital, totalHospital);
+            
+            //We couldn't find the urban area in the data, we use Nominatim to find it
+//            else {
+//                var dataFound = false;
+//                console.log(communityName);
+//                d3.json("http://nominatim.openstreetmap.org/search?q="+communityName+"&state=ma&format=json&addressdetails=1", (function(communityName, nbHospital, totalHospital) {
+//                    return function(error, json) {
+//                        if(json.length > 0) {
+//                            //We loop through the different results until we find the urban area
+//                            for(var j = 0; j < json.length; j++) {
+//                                //We first try to find the data with the city provided
+//                                if(json[j].address != null && json[j].address.city != null) {
+//                                    //We found it!
+//                                    if(getUrbanAreaData(json[j].address.city) != null) {
+//                                        displayUrbanArea(nbHospital, totalHospital);
+//                                        return;
+//                                    } 
+//                                }
+//
+//                                //We then try to find the data with the county
+//                                if(!dataFound && json[j].address != null && json[j].address.county != null) {
+//                                    var countyName = json[j].address.county;
+//                                    countyName = countyName.toLowerCase().replace("county", "");
+//                                    //We found it!
+//                                    if(getUrbanAreaData(countyName) != null) {
+//                                        displayUrbanArea(nbHospital, totalHospital);
+//                                        return;
+//                                    } 
+//                                }
+//                            }
+//
+//                            //In case, we couldn't find anything
+//                            console.log("Unable to retrieve the data for: "+communityName);
+//                        }
+//
+//                        //Nominatim couldn't find the place
+//                        else
+//                            console.log("Unable to retrieve the data for: "+communityName);
+//                    }
+//                })(communityName, nbHospital, totalHospital));
+//            }
+        }
+    }
+    
+    //We add a new panel we the data from the hospital
     var panel = d3.select("body")
       .append("div")
       .attr("class", "panel")
@@ -117,84 +203,24 @@ function topCommunityFromHospital(d) {
     panel.append("h1")
          .text(d.name);
     
-    for(var i = 0; i < hospital.topCommunities.length; i++) {
-        var div = panel.append("div")
-            .attr("class", "hospital");
-        div.append("h2")
-           .text(hospital.topCommunities[i].name+", "+hospital.topCommunities[i].state)
-        div.append("h3")
-           .text(hospital.topCommunities[i].patients+" patients");
-    }
+    var hospitalList = panel.selectAll("div.hospital")
+         .data(urbanAreasLayerId)
+         .enter()
+         .append("div")
+         .attr("class", "hospital")
+         .append("h2")
+         .text(function(d, i) {
+             return hospital.topCommunities[i].name+", "+hospital.topCommunities[i].state;
+         })
+         .append("h3")
+         .text(function(d, i) {
+             return hospital.topCommunities[i].patients+" patients";
+         });
     
-//    var popup = L.popup({closeOnClick: false, closeButton: true})
-//      .setLatLng([d.latitude, d.longitude])
-//      .setContent(d.name)
-//      .openOn(map);
-//    
-    
-    
-    //We calculate the number of patients of that hospital
-    var totalHospital = 0;
-    for(var i = 0; i < hospital.topCommunities.length; i++)
-        totalHospital += hospital.topCommunities[i].patients;
-    
-    for(var i = 0; i < hospital.topCommunities.length; i++) {
-        
-        //Only in the case the city in inside MA
-        if(hospital.topCommunities[i].state == "MA") {
-            var nbHospital =  hospital.topCommunities[i].patients;
-            communityName = hospital.topCommunities[i].name.toLowerCase().trim();
-            
-            //We retrieve the data from urbanAreaData
-            urbanAreaObject = getUrbanAreaData(communityName);
-            
-            if(urbanAreaObject != null)
-                displayUrbanArea(nbHospital, totalHospital);
-            
-            //We couldn't find the urban area in the data, we use Nominatim to find it
-            else {
-                var dataFound = false;
-                console.log(communityName);
-                d3.json("http://nominatim.openstreetmap.org/search?q="+communityName+"&state=ma&format=json&addressdetails=1", (function(communityName, nbHospital, totalHospital) {
-                    return function(error, json) {
-                        if(json.length > 0) {
-                            //We loop through the different results until we find the urban area
-                            for(var j = 0; j < json.length; j++) {
-                                //We first try to find the data with the city provided
-                                if(json[j].address != null && json[j].address.city != null) {
-                                    //We found it!
-                                    if(getUrbanAreaData(json[j].address.city) != null) {
-                                        dataFound = true;
-                                        displayUrbanArea(nbHospital, totalHospital);
-                                        return;
-                                    } 
-                                }
-
-                                //We then try to find the data with the county
-                                if(!dataFound && json[j].address != null && json[j].address.county != null) {
-                                    var countyName = json[j].address.county;
-                                    countyName = countyName.toLowerCase().replace("county", "");
-                                    //We found it!
-                                    if(getUrbanAreaData(countyName) != null) {
-                                        dataFound = true;
-                                        displayUrbanArea(nbHospital, totalHospital);
-                                        return;
-                                    } 
-                                }
-                            }
-
-                            //In case, we couldn't find anything
-                            console.log("Unable to retrieve the data for: "+communityName);
-                        }
-
-                        //Nominatim couldn't find the place
-                        else
-                            console.log("Unable to retrieve the data for: "+communityName);
-                    }
-                })(communityName, nbHospital, totalHospital));
-            }
-            
-        }
-    }
-    
+    panel.selectAll("div.hospital").on("mouseover", function(d) {
+            setUrbanAreaLayerstrokeColor(d, "#C71467", .8, 2);
+         })
+         .on("mouseout", function(d) {
+            setUrbanAreaLayerstrokeColor(d, "#C71467", 0, 2);
+         });
 }
